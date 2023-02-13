fc4a76cbe75a4e1ab651f7830425cf23b1f35755
ehddk <86900169+ehddk@users.noreply.github.com>
2023-02-13 23:18:09 +0900

    프로모션 까지
    스타벅스 페이지 구현

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -12,3 +12,58 @@ searchInputEl.addEventListener("blur", function () {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
+
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    console.log(window.scrollY); //화면이 위에서부터 몇 px 지점에 위치해있는지? 확인 가능
+    if (window.scrollY > 500) {
+      //배지 숨기기
+      // gsap.to(요소, 지속시간, 옵션);
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
+
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach(function (fadeEl, index) {
+  //fadein이라는 요소들의 갯수만큼 forEach에 인수에 적을 함수가 실행됨.
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1,
+  });
+});
+
+// new Swiper(선택자,옵션)
+new Swiper(".notice-line .swiper-container", {
+  direction: "vertical",
+  autoplay: true,
+  loop: true,
+});
+new Swiper(".promotion .swiper-container", {
+  slidesPerView: 3, //한번에 보여줄 슬라이드개수
+  spaceBetween: 10, //슬라이드 사이 여백
+  centeredSlides: true, //1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".promotion .swiper-pagination", //페이지 번호 요소 선택자
+    clickable: true,
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
+});
